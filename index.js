cbf80d0d105a7c77dc8c12ede0c4d7e873ec6e90
unknown <syedaayeshahere0@gmail.com>
2024-04-01 08:34:00 +0500

    Adding a Simple CLI Project

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+let answers = await inquirer.prompt([
+    { message: "Enter Your First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Your Second Number", type: "number", name: "secondtNumber" },
+    {
+        message: "Select One Operator To Perform Operation",
+        type: "list",
+        name: "Operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answers.Operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondtNumber);
+}
+else if (answers.Operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondtNumber);
+}
+else if (answers.Operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondtNumber);
+}
+else if (answers.Operator === "Division") {
+    console.log(answers.firstNumber / answers.secondtNumber);
+}
+else {
+    console.log("invalid input");
+}
+;
